7b52d67d62b5d15bf5a2a6c2f56bcf59181e9c85
HamzaAyyad <hamzaha.ayad1@gmail.com>
2022-10-02 17:16:31 +0300

    sunday-2/10

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,46 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+
+
 import { AppComponent } from './app.component';
+import { CardListComponent } from './card-list/card-list.component';
+import { CardItemComponent } from './card-list/card-item/card-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardListComponent,
+    CardItemComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FontAwesomeModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FlexLayoutModule,
+
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTabsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/card-list/card-item/card-item.component.ts
@@ -0,0 +1,35 @@
+import { ViewEncapsulation } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { faCheckCircle } from '@fortawesome/free-regular-svg-icons'; 
+
+
+@Component({
+  selector: 'app-card-item',
+  templateUrl: './card-item.component.html',
+  styleUrls: ['./card-item.component.css']
+})
+export class CardItemComponent implements OnInit {
+  faCheck = faCheckCircle;
+  selectedDate = new FormControl();
+  SurveyPeriods1 = [{
+    ID:1,
+    START_DATE: '20/3/2021',
+    END_DATE: '30/3/2021'
+  }]
+  SurveyPeriods2 = [{
+    ID:1,
+    START_DATE: '20/3/2021',
+    END_DATE: '30/3/2021'
+  },{
+    ID:2,
+    START_DATE: '1/4/2021',
+    END_DATE: '10/4/2021'
+  }];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
